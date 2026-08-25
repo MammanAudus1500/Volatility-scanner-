5e0f8eb8d5908af77161c02fc9a7236b49063c15
MammanAudus1500 <abubakarmammanaudu3@gmail.com>
2026-08-25 11:02:44 +0100

    Update test messages in test_deriv.py

--- test_deriv.py
+++ test_deriv.py
@@ -1,6 +1,3 @@
-import requests
-
-url = "https://api.deriv.com"
-
 print("Hello! Deriv scanner test is starting...")
-print("If you can see this message, our Python file works.")
+print("Python is working correctly.")
+print("Our scanner robot is alive! 🤖")
